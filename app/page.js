29b043d1e3614b59eb832afc669d150261d42bae
Plopkoekk <perquy.k@gmail.com>
2024-11-21 21:38:42 +0100

    finished styling of navbar and made generation of dropdowns functional with objects.

--- app/page.js
+++ app/page.js
@@ -1,10 +1,26 @@
 import React from 'react'
-import TestComponent from "@/components/TestComponent";
+import './page.css'
 
 const Page = () => {
     return (
         <>
-        <TestComponent/>
+            <div className="content-div">
+                <div className="content-header">
+                    <h1>Caselogger</h1>
+                    <h6 className='subtitle'>Ik maak caselogs alsof ze voor mezelf zouden zijn.</h6>
+                </div>
+                <hr/>
+                <div className="content-body">
+                    <p>Lorem ipsum odor amet, consectetuer adipiscing elit.
+                        Porta sit rhoncus, pellentesque neque ipsum convallis.
+                        Tortor massa convallis torquent sit diam sagittis nullam.
+                        Tempor ac ridiculus maecenas urna aenean primis. Elit dui
+                        primis gravida senectus; pellentesque blandit luctus. Ultricies
+                        pellentesque congue dictumst commodo feugiat proin non convallis
+                        ligula. Varius taciti cubilia parturient quis quam ullamcorper
+                        lacus sit.</p>
+                </div>
+            </div>
         </>
     )
 }
--- components/header/Header.jsx
+++ components/header/Header.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
-import NavDefault from '@/components/navDefault/NavDefault'
 import Nav from '@/components/nav/Nav'
+
 
 function Header() {
     return (
