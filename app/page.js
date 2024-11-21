d4a0d8780a56fe05e1e88b8c50aa860c7b241f0b
Plopkoekk <perquy.k@gmail.com>
2024-11-21 22:17:36 +0100

    Added content to Homepage

--- app/page.js
+++ app/page.js
@@ -1,25 +1,29 @@
 import React from 'react'
 import './page.css'
-
+import Changelog from '@/components/changelog/Changelog'
+import Image from "next/image";
 const Page = () => {
     return (
         <>
             <div className="content-div">
                 <div className="content-header">
-                    <h1>Caselogger</h1>
+                    <div className="flex-title">
+                        <Image className="header-logo" src="/Logo50.png" alt="Logo" width={50} height={50}/>
+                        <h1>Caselogger</h1>
+                    </div>
                     <h6 className='subtitle'>Ik maak caselogs alsof ze voor mezelf zouden zijn.</h6>
                 </div>
                 <hr/>
                 <div className="content-body">
-                    <p>Lorem ipsum odor amet, consectetuer adipiscing elit.
-                        Porta sit rhoncus, pellentesque neque ipsum convallis.
-                        Tortor massa convallis torquent sit diam sagittis nullam.
-                        Tempor ac ridiculus maecenas urna aenean primis. Elit dui
-                        primis gravida senectus; pellentesque blandit luctus. Ultricies
-                        pellentesque congue dictumst commodo feugiat proin non convallis
-                        ligula. Varius taciti cubilia parturient quis quam ullamcorper
-                        lacus sit.</p>
+                    <p className="home-text">Kies bovenaan de gewenste template <br/>
+                    Vul het formulier in en druk op de Copy of Download knop.<br/>
+                    Easy-Peasy.</p>
+                    <hr/>
+                    <Changelog />
+                    <hr/>
+                    <p className="home-text">Bij problemen: stuur Kenny een Whatsappje 😊</p>
                 </div>
+
             </div>
         </>
     )
